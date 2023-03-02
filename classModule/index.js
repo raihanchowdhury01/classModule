2d84b962f8a31a18d5c139ea4bad12afb14a797b
raihanchowdhury01 <rakibc698@gmail.com>
2023-03-02 23:35:52 +0600

    second

---
+++ classModule/index.js
@@ -0,0 +1,32 @@
+/*
+//class 32(2)No. video
+const users = [
+    {id: 1, name: 'abul', job: 'doctor'}
+
+]
+const user = {
+    count: 4000,
+    date: {
+        co: 200, 
+        dat:{
+            counte: 100,
+            da: [
+                {id: 1}, {id: 2}, {id:3}
+            ]
+        }
+    }
+}
+
+console.log(user.date.dat.da[1].id);
+console.log(users[0].name);
+
+//class 32(3)No.Video
+const numbers = [2, 8, 4, 6, 3];
+const double = num => num * 2;
+const makeDouble = numbers.map(double);
+
+console.log(makeDouble);
+*/
+
+
+//class 32(4)No. Video
